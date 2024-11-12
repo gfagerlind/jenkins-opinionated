1c9e58d98794f0e1dd02f67d9754618fc4dfb6b7
Gustav Fägerlind <gustav.fagerlind@ictech.se>
2024-11-12 14:05:58 +0100

    Clean up Jenkinsfile and bootstrap

---
+++ libs/ImportFile.groovy
@@ -0,0 +1,33 @@
+// boilerplate to get jenkins started
+_main = []
+def call(file) {
+    def _import_file = {
+        obj = load("${pwd()}/${file}")
+        if (obj instanceof java.util.LinkedHashMap) {
+            // Most things runs just in init phase,
+            // but for a cleaner log look, we can add a main step,
+            // and put things there instead.
+            if (obj.main) {
+                _main += obj.main
+                obj.main.delegate = this
+            }
+            if (obj.init) {
+                obj.init.delegate = this
+                obj.init()
+            }
+
+        } else {
+            obj.delegate = this
+            obj()
+        }
+    }
+    if (! env.NODE_NAME) {
+        node {
+            checkout scm
+            _import_file()
+        }
+    } else {
+        _import_file()
+    }
+}
+return this
--- top.groovy
+++ top.groovy
@@ -1,15 +1,19 @@
 // top jenkins file, without boilerplate
 { ->
-    declareParameter(string(name: 'JOB_TYPE', defaultValue: ''))
-    env.JOB_TYPE = params.JOB_TYPE
-    if (params.JOB_TYPE == "d") {
-        import_file("dep.groovy")
-    }
-    if (params.JOB_TYPE == 'p') {
-        import_file("postmerge.groovy")
-    }
-    if (params.JOB_TYPE == 'c') {
-        import_file("check.groovy")
+    import_file('./libs/DeclareParameter.groovy')
+    withParameters()
+    {
+        declareParameter(string(name: 'JOB_TYPE', defaultValue: ''))
+        env.JOB_TYPE = params.JOB_TYPE
+        if (params.JOB_TYPE == "d") {
+            import_file("dep.groovy")
+        }
+        if (params.JOB_TYPE == 'p') {
+            import_file("postmerge.groovy")
+        }
+        if (params.JOB_TYPE == 'c') {
+            import_file("check.groovy")
+        }
     }
     // What about not dependent gate? - that is trivial, just remove the dependentGate closure,
     // What if i want to do wacky combinations of stageWithWip and dependentGate? - go right ahead, but i dont support dependentGate in dependentGate because i dont know what it would mean
