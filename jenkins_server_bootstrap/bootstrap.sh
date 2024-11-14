faa7552684bb60df26c279b61cc28e9605e443ff
Gustav Fägerlind <gustav.fagerlind@ictech.se>
2024-11-14 07:55:33 +0100

    Fix bootstrap

--- jenkins_server_bootstrap/bootstrap.sh
+++ jenkins_server_bootstrap/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -ue -o pipefail
-JENKINS_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/jenkins_home
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+JENKINS_HOME="${SCRIPT_DIR}/jenkins_home"
 GITROOT=$(git rev-parse --show-toplevel)
 PORT=80
 
-docker build . -t jo
+docker build -t jo ${SCRIPT_DIR}
 docker run \
     --rm \
     -p ${PORT}:8080 \
